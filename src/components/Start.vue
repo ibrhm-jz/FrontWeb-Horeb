e10c52888c323d83de5e6fed61221ad854379031
surii <isurilop@gmail.com>
2020-09-18 13:35:40 -0500

    cambios-web

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,36 +1,38 @@
 <template>
   <div>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="light" fixed="top">
+        <b-navbar-brand href="#">
+          <img src="../assets/logo-nuevo.png" alt width="160" height="55" />
+        </b-navbar-brand>
 
-  <div>
-  <b-navbar toggleable="lg" type="dark" variant="light" fixed="top">
-    <b-navbar-brand href="#"><img src="../assets/logo-nuevo.png" alt width="160" height="55" /></b-navbar-brand>
-
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#" color=blue><font color="black">Home</font></b-nav-item>
-       <b-nav-item href="#" color=blue ><font color="black">Productos</font></b-nav-item>
-       <b-nav-item href="#" color=blue><font color="black">Servicios</font></b-nav-item>
-       <b-nav-item href="#" color=blue><font color="black">Nosotros</font></b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-
-
-      
-          <router-link link to="/login">
-            <button class="btn btn-light" type="submit" right>
-              Iniciar Sesion
-              <b-icon icon="lock-fill" aria-hidden="true"></b-icon>
-            </button>
-          </router-link>
-       
-      </b-navbar-nav>
-    </b-collapse>
-
-  </b-navbar>
-</div>
-
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#" color="blue">
+              <font color="black">Inicio</font>
+            </b-nav-item>
+            <b-nav-item href="#" color="blue">
+              <font color="black">Productos</font>
+            </b-nav-item>
+            <b-nav-item href="#" color="blue">
+              <font color="black">Servicios</font>
+            </b-nav-item>
+            <b-nav-item href="#" color="blue">
+              <font color="black">Nosotros</font>
+            </b-nav-item>
+          </b-navbar-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <router-link link to="/login">
+              <button class="btn btn-light" type="submit" right>
+                Iniciar Sesion
+                <b-icon icon="lock-fill" aria-hidden="true"></b-icon>
+              </button>
+            </router-link>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
 
     <div>
       <v-carousel :show-arrows="false" cycle="true">
@@ -45,135 +47,208 @@
     </div>
     <v-spacer></v-spacer>
     <br />
+    <div align="center">
+      <b>
+        <font color="black" size="5">Productos</font>
+        <font color="#00b686" size="5">&nbsp;Destacados</font>
+      </b>
+      <hr style="width:90%;" />
+    </div>
+    <br />
+    <div class="contenedor">
+      <div class="row">
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url1" height="200px"></v-img>
+          <v-card-title>Tuberia Corrugada</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
 
-    <div class="contenedor" >
-      <div align="center" class="row">
-        <div class="col-sm-4">
-          <div>
-            <b-card
-              title="Alcantarillado"
-              :img-src="url1"
-              img-alt="Image"
-              img-height="300"
-              img-top
-              tag="article"
-              style="max-width: 20rem; "
-              class="mb-2"
-            >
-              <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url2" height="200px"></v-img>
+          <v-card-title>Pasto Cintetico</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
 
-              <b-button href="#" class="btn-green">Conocer mas</b-button>
-            </b-card>
-          </div>
-        </div>
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url3" height="200px"></v-img>
+          <v-card-title>Valvuleria</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
 
-        <div class="col-sm-4">
-          <div>
-            <b-card
-              title="Pasto sintetico"
-              :img-src="url2"
-              img-alt="Image"
-                            img-height="300"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-            
-              <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
-
-              <b-button href="#" class="btn-green">Conocer mas</b-button>
-            </b-card>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div>
-            <b-card
-              title="Valvulas"
-             :img-src="url3"
-              img-alt="Image"
-              
-              img-height="300"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-              <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
-
-              <b-button href="#" class="btn-green">Conocer mas</b-button>
-            </b-card>
-          </div>
-        </div>
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url4" height="200px"></v-img>
+          <v-card-title>Geomembrana</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
       </div>
     </div>
-    <br>
-        <div class="contenedor" >
-      <div align="center" class="row">
-        <div class="col-sm-4">
-          <div>
-            <b-card
-              title="Geomembrana"
-              :img-src="url4"
-              img-alt="Image"
-              img-height="300"
-              img-top
-              tag="article"
-              style="max-width: 20rem; "
-              class="mb-2"
-            >
-              <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
+    <br />
+    <div class="contenedor">
+      <div class="row">
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url5" height="200px"></v-img>
+          <v-card-title>Tuberia de Pvc</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
 
-              <b-button href="#" class="btn-green" >Conocer mas</b-button>
-            </b-card>
-          </div>
-        </div>
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url6" height="200px"></v-img>
+          <v-card-title>Plantas de Tratamiento</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
 
-        <div class="col-sm-4">
-          <div>
-            <b-card
-              title="Tuberia potable"
-              :img-src="url5"
-              img-alt="Image"
-                            img-height="300"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-            
-              <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url7" height="200px"></v-img>
+          <v-card-title>Tuberia de acero</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
 
-              <b-button href="#" class="btn-green">Conocer mas</b-button>
-            </b-card>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div>
-            <b-card
-              title="Plantas de tratamiento"
-             :img-src="url6"
-              img-alt="Image"
-              
-              img-height="300"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2"
-            >
-              <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
-
-              <b-button href="#" class="btn-green">Conocer mas</b-button>
-            </b-card>
-          </div>
-        </div>
+        <v-card class="mx-auto" max-width="250">
+          <v-img :src="url8" height="200px"></v-img>
+          <v-card-title>Piezas Especiales</v-card-title>
+          <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="orange" text>ver mas</v-btn>
+          </v-card-actions>
+        </v-card>
       </div>
     </div>
+    <br />
+    <br />
+    <div class="contenedor-middle">
+      <br />
+
+      <div align="left" class="contenedor">
+        <div class="row">
+          <div class="footer" style="width:25%;">
+            <br />
+            <h4>
+              <b-icon icon="file-earmark-plus-fill" color="white"></b-icon>&nbsp;Certificaciones
+            </h4>
+            <br />Contamos con certificaciones avaladas para los respectivos tubos.
+            <br />
+          </div>
+
+          <div class="footer" style="width:25%;">
+            <br />
+            <h4>
+              <b-icon icon="hammer" color="white"></b-icon>&nbsp;Servicios
+            </h4>
+            <br />Instalacion de Enmallado Perimetral
+            <br />Instalacion en Pasto cintetico
+            <br />Instalacion de Concertina
+            <br />Instalacion de Geomembrana
+            <br />
+            <br />
+            <b-button class="btn-green">Conoce mas</b-button>
+          </div>
+
+          <div class="footer" style="width:25%;">
+            <br />
+            <h4>
+              <b-icon icon="headset" color="white"></b-icon>&nbsp;Asesoria
+            </h4>
+            <br />Contamos con vendedores especializados para asesorlos con nuestros productos.
+            <br />
+          </div>
+        </div>
+        <br />
+      </div>
+
+      <div>
+        <b-modal v-model="modalShow">Hello From Modal!</b-modal>
+      </div>
+    </div>
+
     <br>
+          <div align="left" class="contenedor">
+        <div class="row">
+          <div class="footer" style="width:30%;">
+            <br />
+            <v-img :src="pensative" width="250"></v-img>
+            <br />
+          </div>
+
+
+          <div align="center" class="footer" style="width:30%;">
+            <br />
+            <br />
+            <h3>
+             ¿Tienes dudas?
+            </h3>
+            <br>
+            Contactanos, y podremos ayudarte.
+            <br /> <br />
+              <b-button class="btn-green">Conoce mas</b-button>
+          </div>
+        </div>
+        <br />
+      </div>
+    <br />
+    <br />
     <div class="contenedor-play">
+      <br />
+      <br />
+      <h4 style="margin-left:13%">
+        <b-icon icon="arrow-right-short"></b-icon>Contactanos
+      </h4>
+      <div align="center" class="contenedor">
+        <div class="row">
+          <div class="footer" style="width:25%;">
+            <br />
+            <h5>Direccion:</h5>
+            <hr color="white" />2a. Ote.Nte #682-B entre 5a y 6a norte, Col.Centro Tuxtla Gutiérrez,Chis.
+            <br />
+            <br />
+          </div>
 
+          <div class="footer" style="width:25%; ">
+            <br />
+            <h5>Datos de contacto:</h5>
+            <hr color="white" />Ofic.(961) 118 85 80 / Cel.961 196 30 20
+            <v-spacer></v-spacer>Tuberia_horeb@hotmail.com
+            <br />
+            <br />
+          </div>
+
+          <div class="footer" style="width:25%; ">
+            <br />
+            <h5>Redes Sociales:</h5>
+            <hr color="white" />2a. Ote.Nte #682-B entre 5a y 6a norte, Col.Centro Tuxtla Gutiérrez,Chis.
+            <br />
+            <br />
+          </div>
+        </div>
+        <br />
+        <font size="2">Copyright 2020 Suministros en Tuberia Horeb, S.A. de C.V.</font>
+        <br />
+        <br />
+      </div>
+
+      <div>
+        <b-modal v-model="modalShow">Hello From Modal!</b-modal>
+      </div>
     </div>
   </div>
 </template>
@@ -181,6 +256,8 @@
 export default {
   data() {
     return {
+      modalShow: false,
+      show: false,
       items: [
         {
           src: require("../assets/tapas.jpg"),
@@ -203,9 +280,12 @@ export default {
       url1: require("../assets/1-p.png"),
       url2: require("../assets/pasto.jpg"),
       url3: require("../assets/2-p.png"),
-      url4: require("../assets/3-p.png"),
+      url4: require("../assets/8-p.jpeg"),
       url5: require("../assets/4-p.png"),
       url6: require("../assets/5-p.jpg"),
+      url7: require("../assets/6-p.jpg"),
+      url8: require("../assets/7-p.png"),
+      pensative: require("../assets/pensative.png"),
     };
   },
 };
@@ -220,11 +300,27 @@ export default {
   padding: 8px;
 }
 .contenedor {
-  width: 70%;
-  margin-left: 15%;
-  margin-right: 15%;
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
 }
-.contenedor-play{
+.contenedor-play {
+  background-color: #00b686;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
+}
+.card-home {
+  width: 20%;
+  padding: 8px;
+}
+.footer {
+  margin-left: 5%;
+  margin-right: 2%;
+}
 
+.contenedor-middle {
+  background-color: #c6c6c6;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
 }
 </style>
